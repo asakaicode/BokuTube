6a112b53052ae93f4317109efebd9d9b2b2cd3f4
asakaicode <tubakaito5100@gmail.com>
2025-08-11 18:14:11 +0900

    fix: introduce ApolloServerPluginLandingPageLocalDefault()

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,15 @@
 import { ApolloServer } from '@apollo/server'
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default'
 import { startStandaloneServer } from '@apollo/server/standalone'
 import apolloConfig from '~/schema/index.js'
 
 const PORT = 4000
 
 const runServer = async () => {
-  const server = new ApolloServer(apolloConfig)
+  const server = new ApolloServer({
+    ...apolloConfig,
+    plugins: [ApolloServerPluginLandingPageLocalDefault()],
+  })
   const { url } = await startStandaloneServer(server, {
     listen: { port: PORT },
   })
