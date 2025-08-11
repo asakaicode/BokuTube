f30809e3e3cabad3562a7921aaebd77b68a0d1ec
asakaicode <tubakaito5100@gmail.com>
2025-08-12 04:41:37 +0900

    fix: add typeDefs and resolvers to overall

--- backend/src/schema/index.ts
+++ backend/src/schema/index.ts
@@ -1,12 +1,15 @@
+import commonSchema from '~/schema/common/index.js'
 import videoSchema from '~/schema/videos/index.js'
 
-const typeDefs = [videoSchema.typeDefs]
+const typeDefs = [commonSchema.typeDefs, videoSchema.typeDefs]
 
 const resolvers = {
   Query: {
+    ...commonSchema.resolvers.Query,
     ...videoSchema.resolvers.Query,
   },
   Mutation: {
+    ...commonSchema.resolvers.Mutation,
     ...videoSchema.resolvers.Mutation,
   },
 }
