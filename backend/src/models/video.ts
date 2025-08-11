4b0ae70dd5443006796631db2628682d11121256
asakaicode <tubakaito5100@gmail.com>
2025-08-11 19:04:14 +0900

    feat: introduce mock functions

--- backend/src/models/video.ts
+++ backend/src/models/video.ts
@@ -8,3 +8,50 @@ export interface Video {
   createdAt: Date
   visibility: string
 }
+
+export const mockVideos: Video[] = [
+  {
+    id: '1',
+    title: '1984',
+    description: 'Dystopian novel',
+    durationSec: 3600,
+    playbackUrl: 'http://example.com/1984',
+    thumbnailUrl: 'http://example.com/1984.jpg',
+    createdAt: new Date(),
+    visibility: 'public',
+  },
+  {
+    id: '2',
+    title: 'To Kill a Mockingbird',
+    description: 'Novel about racial injustice',
+    durationSec: 4200,
+    playbackUrl: 'http://example.com/to-kill-a-mockingbird',
+    thumbnailUrl: 'http://example.com/to-kill-a-mockingbird.jpg',
+    createdAt: new Date(),
+    visibility: 'public',
+  },
+  {
+    id: '3',
+    title: 'The Great Gatsby',
+    description: 'Novel set in the Jazz Age',
+    durationSec: 3600,
+    playbackUrl: 'http://example.com/the-great-gatsby',
+    thumbnailUrl: 'http://example.com/the-great-gatsby.jpg',
+    createdAt: new Date(),
+    visibility: 'public',
+  },
+]
+
+export const mockFetchVideo = async (id: string): Promise<Video | null> => {
+  // Simulate a database call
+  await new Promise((resolve) => setTimeout(resolve, 100))
+
+  return mockVideos.find((video) => video.id === id) ?? null
+}
+
+export const mockFetchVideos = async (): Promise<Video[]> => {
+  // Simulate a database call
+  await new Promise((resolve) => setTimeout(resolve, 100))
+
+  return mockVideos
+}
---
+++ backend/src/schema/videos/queries/video.ts
@@ -0,0 +1,15 @@
+import { GraphQLResolveInfo } from 'graphql'
+import { mockFetchVideo, Video } from '~/models/video.js'
+
+export interface VideoArgs {
+  id: string
+}
+
+export default async function video(
+  _parent: unknown,
+  input: VideoArgs,
+  _context: unknown,
+  _info: GraphQLResolveInfo,
+): Promise<Video | null> {
+  return await mockFetchVideo(input.id)
+}
