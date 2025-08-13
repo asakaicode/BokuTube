6ab8b77842737f196cc37b42a7bc7bcb54fb5f04
asakaicode <tubakaito5100@gmail.com>
2025-08-14 00:44:30 +0900

    feat: add custom graphql client

---
+++ frontend/src/utils/graphql.ts
@@ -0,0 +1,57 @@
+import {
+  useMutation,
+  UseMutationOptions,
+  UseMutationResult,
+  useQuery,
+  useQueryClient,
+  UseQueryResult,
+} from '@tanstack/react-query'
+
+export enum GRAPHQL_OPERATION {
+  QUERY = 'query',
+  MUTATION = 'mutation',
+}
+
+export const useGraphQLQuery = <T>(
+  query: string,
+  variables: Record<string, unknown>,
+): UseQueryResult<T> => {
+  return useQuery({
+    queryKey: [query, variables],
+    queryFn: () => fetchWithGraphQL<T>(query, variables),
+  })
+}
+
+export const useGraphQLMutation = <
+  TData,
+  TVariables extends Record<string, unknown>,
+>(
+  mutation: string,
+  options?: Omit<UseMutationOptions<TData, Error, TVariables>, 'mutationFn'>,
+): UseMutationResult<TData, Error, TVariables> => {
+  const queryClient = useQueryClient()
+  return useMutation<TData, Error, TVariables>({
+    mutationFn: (variables: TVariables) =>
+      fetchWithGraphQL<TData>(mutation, variables),
+    ...options,
+  })
+}
+
+const fetchWithGraphQL = async <T>(
+  query: string,
+  variables: Record<string, unknown>,
+): Promise<T> => {
+  const res = await fetch('/graphql', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ query, variables }),
+  })
+
+  const json = await res.json()
+
+  if (json.errors) {
+    throw new Error(JSON.stringify(json.errors))
+  }
+
+  return json.data as T
+}
