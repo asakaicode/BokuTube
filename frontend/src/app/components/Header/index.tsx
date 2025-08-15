a9e91b4a6d0d67454b6036c824ae0cf24d00a519
asakaicode <tubakaito5100@gmail.com>
2025-08-16 06:41:53 +0900

    feat: add MainLogo

--- frontend/src/app/components/Header/index.tsx
+++ frontend/src/app/components/Header/index.tsx
@@ -1,15 +1,16 @@
-"use client"
+'use client'
 
 import { TextField, Typography } from '@mui/material'
 import React from 'react'
-import GridContainer from '~/components/Grid/GridContainer'
 import GridItem from '~/components/Grid/GridItem'
+import MainLogo from '~/components/Icons/MainLogo'
+import HStack from '~/components/Stack/HStack'
 
 const Header: React.FC = () => {
   return (
-    <GridContainer tailwindStyle="h-[54px]">
-      <GridItem size={4}>
-        <Typography variant="h1">BokuTube</Typography>
+    <HStack spacing={2}>
+      <GridItem size={4} tailwindStyle="w-[240px] h-[54px]">
+        <MainLogo />
       </GridItem>
       <GridItem size={4}>
         <TextField />
@@ -17,7 +18,7 @@ const Header: React.FC = () => {
       <GridItem size={4}>
         <Typography>aaa</Typography>
       </GridItem>
-    </GridContainer>
+    </HStack>
   )
 }
 
